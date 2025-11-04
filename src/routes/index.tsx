5867f0367afeb5da69f7eeca58f608dc63302a02
fzsf163 <yofaisol619@gmail.com>
2025-11-05 01:46:23 +0600

    hook line added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,5 +5,12 @@ export const Route = createFileRoute('/')({
 })
 
 function App() {
-  return <div className="text-center">Hello "/"</div>
+  return (
+    <main className="text-center">
+      {/*hookline*/}
+      <div className="font-hook p-7 text-3xl font-semibold">
+        "Everything is Good, Daijoubu Desuu"
+      </div>
+    </main>
+  )
 }
